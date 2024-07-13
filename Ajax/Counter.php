83e441b799d1d3f49eb932f99a4863036896bdbf
kim-heesuk <linheedol@gmail.com>
2024-07-13 11:45:18 +0900

    counter3

--- Ajax/Counter.php
+++ Ajax/Counter.php
@@ -4,7 +4,6 @@ header("Content-type: application/json; charset=utf-8");
 // 클라이언트 IP 주소와 현재 날짜 가져오기
 if ($_SERVER['DOCUMENT_ROOT']=="/workspace"){
 $ip_address = $_SERVER['REMOTE_ADDR'];
-$ip_address = "10.10.10.10";
 $visit_date = date('Y-m-d');
 include "./conn.php";
 $sql = "SELECT ip_address,visit_date FROM visit.visits WHERE ip_address = :ip_address AND visit_date = :visit_date";
@@ -15,12 +14,10 @@ $stmt->execute();
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC); // 결과를 가져옴
 	if (count($result) == 0) {
 		// 방문 기록이 없으면 새로운 방문자로 간주하고 방문 기록을 추가
-		
 		$sql = "INSERT INTO visit.visits (ip_address, visit_date) VALUES (:ip_address, :visit_date)";
 		$stmt = $conn->prepare($sql);
 		$stmt->bindValue(':ip_address', $ip_address);
 		$stmt->bindValue(':visit_date', $visit_date);
-		
 		$stmt->execute();
 	}
 $sql = "SELECT count(*) as count FROM visit.visits WHERE 1 = 1";
